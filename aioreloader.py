e85513670305ec91d012a0ad73ab5306ed845c96
Andriy Maletsky <andriy.maletsky@gmail.com>
2016-11-09 16:13:22 +0200

    compatibility with 3.4

--- aioreloader.py
+++ aioreloader.py
@@ -1,14 +1,15 @@
 import sys
 import os
-from asyncio import AbstractEventLoop, sleep
+import asyncio
 from types import ModuleType
 
+_abstract_loop = asyncio.AbstractEventLoop
 
 _started = False
 _files = set()
 
 
-def start(loop: AbstractEventLoop, interval=0.5):
+def start(loop: _abstract_loop, interval=0.5):
     global _started
     if _started:
         return
@@ -22,10 +23,11 @@ def watch(path):
     _files.add(path)
 
 
-def _call_periodically(loop: AbstractEventLoop, interval, callback, *args):
-    async def wrap():
+def _call_periodically(loop: _abstract_loop, interval, callback, *args):
+    @asyncio.coroutine
+    def wrap():
         while True:
-            await sleep(interval)
+            yield from asyncio.sleep(interval)
             callback(*args)
     return loop.create_task(wrap())
 
