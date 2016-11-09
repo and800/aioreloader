4c9a2cfc930f1d4b34fcc55cd0514581340be9c2
Andriy Maletsky <andriy.maletsky@gmail.com>
2016-11-09 15:27:00 +0200

    make repo look like repo

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='aioreloader',
+    version='0.0.0',
+    description='Port of tornado reloader to asyncio',
+    url='https://github.com/and800/aioreloader',
+    author='Andriy Maletsky',
+    author_email='andriy.maletsky@gmail.com',
+    license='MIT',
+    py_modules=['aioreloader'],
+)
